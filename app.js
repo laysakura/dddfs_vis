598164984c7bd83be0806fc7faa0304e68943e3c
Sho Nakatani <lay.sakura@gmail.com>
2012-05-25 16:39:56 +0900

    Changed usable nodes
    Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(8124, function(){
+app.listen(11111, function(){
     console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
